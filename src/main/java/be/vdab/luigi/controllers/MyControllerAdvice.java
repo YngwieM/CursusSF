2937fd93ee349c1daa1510a16884478814fb1594
Yngwie Mariman <yngwie.mariman@vdabcampus.be>
2020-10-26 09:23:11 +0100

    Cursus

--- src/main/java/be/vdab/luigi/controllers/MyControllerAdvice.java
+++ src/main/java/be/vdab/luigi/controllers/MyControllerAdvice.java
@@ -2,9 +2,10 @@ package be.vdab.luigi.controllers;
 
 import be.vdab.luigi.sessions.Identificatie;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-public class MyControllerAdvice {
+@ControllerAdvice
+ class MyControllerAdvice {
     private final Identificatie identificatie;
     MyControllerAdvice(Identificatie identificatie) {
         this.identificatie = identificatie;
